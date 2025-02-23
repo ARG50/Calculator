8c162b77b0dd4b0089bfad0f6af0b081b1f09fc8
ARG50 <arghya.samanta50@gmail.com>
2025-02-23 11:14:43 +0530

    Calculator modified with display panel & buttons.
    1. Frame display panel Implemented towards North.
    2. String array of symbols initiated.
    3. Button panel initiated with symbol array.

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,5 +5,29 @@ import java.util.Arrays;
 import javax.swing.*;
 
 public class Calculator {
-    WindowFrame myFrame = new WindowFrame();
+
+    WindowFrame myFrame;
+
+    JPanel buttonPanel = new JPanel();
+
+    String[] buttonValues = {
+            "AC", "+/-", "%", "÷",
+            "7", "8", "9", "×",
+            "4", "5", "6", "-",
+            "1", "2", "3", "+",
+            "0", ".", "√", "="
+    };
+
+    String[] rightSymbols = {"÷", "×", "-", "+", "="};
+    String[] topSymbols = {"AC", "+/-", "%"};
+
+    Calculator()
+    {
+        myFrame = new WindowFrame();
+        buttonPanel.setLayout(new GridLayout(5, 4));
+        buttonPanel.setBackground(myFrame.customBlack);
+        myFrame.add(buttonPanel);
+
+    }
+
 }
--- src/WindowFrame.java
+++ src/WindowFrame.java
@@ -2,18 +2,39 @@ import javax.swing.*;
 import java.awt.*;
 
 public class WindowFrame extends JFrame {
-    int frameWidth = 360;
-    int frameHeight = 550;
+    private int frameWidth = 360;
+    private int frameHeight = 550;
+
+    JPanel displayPanel = new JPanel();
+    JLabel displayLabel = new JLabel();
+
+    // Reference from Iphone Calculator colors.
+    protected Color customLightGrey = new Color(212, 212,210);
+    protected Color customBlack = new Color(28, 28, 28);
+    protected Color customDarkGrey = new Color(80, 80, 80);
+    protected Color customOrange = new Color(255, 149, 0);
 
     WindowFrame()
     {
 //        setPreferredSize(new Dimension(360, 540));
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
-        setSize(frameWidth, frameHeight);
-        setResizable(false);
-        setLocationRelativeTo(null);
-        setTitle("My Calculator");
-        setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new BorderLayout());
+        this.setSize(frameWidth, frameHeight);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        this.setTitle("My Calculator");
+
+        displayLabel.setBackground(customBlack);
+        displayLabel.setForeground(Color.WHITE);
+        displayLabel.setFont(new Font("Arial", Font.PLAIN, 80));
+        displayLabel.setHorizontalAlignment(JLabel.RIGHT);
+        displayLabel.setText("0");
+        displayLabel.setOpaque(true);
+
+        displayPanel.setLayout(new BorderLayout());
+        displayPanel.add(displayLabel);
+        this.add(displayPanel, BorderLayout.NORTH);
+
+        this.setVisible(true);
     }
 }
